e035be8ab78ad33ee0ae12e1766d9641d6edca22
LUCIOZTW <luciomansillaztw@gmail.com>
2022-04-28 10:39:56 -0300

    Test post user

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -10,11 +10,33 @@ class App < Sinatra::Application
     end
   end
 
+  configure do 
+    set :views, 'app/views'
+  end
+
   def initialize(app = nil)
     super()
   end
 
-  get '/' do
-    'holaaaaa!!!!!'
+  user = [
+    {name: 'Juan', email: 'juan@gmail.com',password:'12345'},
+    {name: 'Pedro', email: 'pedro@gmail.com',password:'12345'},
+    {name: 'Ana', email: 'ana@gmail.com',password:'12345'}
+  ]
+
+
+  get '/users2/:name' do
+    user.find { |u| u[:name] == params[:name] }.to_s
   end
+
+  #post
+  post '/users' do
+    user.push(params)
+    user.to_json
+  end
+
+  get '/users' do
+    erb:user
+  end 
+
 end
