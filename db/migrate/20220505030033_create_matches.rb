f10e4fa9e374c1901abd6cd9bf7f5056278a6522
mateovieyra <mateovieyra8@gmail.com>
2022-05-05 00:23:15 -0300

    Fix create_matches migration

--- db/migrate/20220505030033_create_matches.rb
+++ db/migrate/20220505030033_create_matches.rb
@@ -1,10 +1,10 @@
 class CreateMatches < ActiveRecord::Migration[7.0]
   def change
     create_table :matches do |t| 
-      t.string :matches, 
-      t.integer :home_goals,
-      t.integer :away_goals,
-      t.datetime :datetime,
+      t.string :matches
+      t.integer :home_goals
+      t.integer :away_goals
+      t.datetime :datetime
       t.string :type
     end
   end
