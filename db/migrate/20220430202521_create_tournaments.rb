3fa290c81c81837f1a6ba53697c71da8189f383a
BrendaDichiara <brendy-15@hotmail.com>
2022-05-04 23:59:41 -0300

    Update Tournaments

--- db/migrate/20220430202521_create_tournaments.rb
+++ db/migrate/20220430202521_create_tournaments.rb
@@ -1,10 +1,7 @@
 class CreateTournaments < ActiveRecord::Migration[7.0]
   def change
-
     create_table :tournaments do |t|
-      t.integer :id_Tournament
       t.string :name 
     end
-
   end
 end
