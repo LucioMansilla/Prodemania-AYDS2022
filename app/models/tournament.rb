bf15f8c047bddb2e1e5def3624f27b5b979e7344
mateovieyra <mateovieyra8@gmail.com>
2022-05-12 00:27:02 -0300

    Uptade Tournament

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,6 +1,5 @@
 class Tournament < ActiveRecord::Base
     has_and_belongs_to_many :teams
-    has_and_belongs_to_many :players
     has_many :players, through: :points
     has_many :points
     has_many :match_days
