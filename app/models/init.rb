ec0e3205a3b986166b36c4716638e8f9f5c17fd6
BrendaDichiara <brendy-15@hotmail.com>
2022-05-12 01:24:34 -0300

    Add init

---
+++ app/models/init.rb
@@ -0,0 +1,7 @@
+require_relative 'match_day'
+require_relative 'forecast'
+require_relative 'match'
+require_relative 'player'
+require_relative 'point'
+require_relative 'team'
+require_relative 'tournament'
