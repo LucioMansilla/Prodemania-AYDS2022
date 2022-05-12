aedd4ac153ca69af0a74f5c6b753b12af71e51d4
BrendaDichiara <brendy-15@hotmail.com>
2022-05-12 00:21:09 -0300

    Update migration

---
+++ db/migrate/20220512014023_create_teams_tournaments.rb
@@ -0,0 +1,9 @@
+class CreateTeamsTournaments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :teams_tournaments do |t|
+      t.references :team 
+      t.references :tournament
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20220512023348_create_admins.rb
@@ -0,0 +1,9 @@
+class CreateAdmins < ActiveRecord::Migration[7.0]
+  def change
+    create_table :admins do |t|
+      t.string :name, :password
+      t.string :email, index: { unique: true, name: 'unique_emails' }
+      t.timestamps
+    end
+  end
+end
