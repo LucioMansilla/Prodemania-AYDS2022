b9e9ac9fb3500c01c12035a6bb0c9685a202d670
mateovieyra <mateovieyra8@gmail.com>
2022-05-18 17:30:07 -0300

    update forecast and match

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -3,27 +3,34 @@ class Forecast < ActiveRecord::Base
     belongs_to :match
     belongs_to :tournament
 
-    def points
-
-        total_points = 0
-        
-        if self.match.has_result 
-            if self.guess_result
-                total_points += 1
-
-                if self.guess_goals
-                    total_points += 2
-                end
-            end
-        end
-        total_points
+    attr_accessor :old_points
+  
+    def calculate_points
+      
+      self.old_points = self.points ? self.points : 0
+  
+      total_points = 0
+      if self.match.has_result?
+        if self.guess_result 
+          total_points += 1
+    
+          if self.guess_goals
+            total_points += 2
+          end   
+        end 
+      end
+  
+      self.points = total_points
+      self.save
+  
+      total_points
     end
-
+  
     def guess_result
-        self.result == self.match.result
+      self.result == self.match.result  
     end
-
-    def guess_goals
-        self.home_goals == self.match.home_goals and self.away_goals == self.match.away_goals
+  
+    def guess_goals 
+      self.goals_home == self.match.goals_home && self.goals_away == self.match.goals_away
     end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -4,8 +4,33 @@ class Match < ActiveRecord::Base
   belongs_to :home, class_name: "Team"
   belongs_to :away, class_name: "Team"
 
-  def has_result
-    self.result != nil and self.home_goals != nil and self.away_goals != nil 
+  before_update :calculate_points,  if: :result_or_goals_changed?
+
+  def has_result?
+    self.result != nil and self.goals_home != nil and self.goals_away != nil
   end
+
+  def set_result(result, goals_home, goals_away)
+    self.result = result 
+    self.goals_home = goals_home
+    self.goals_away = goals_away
+  end
+
+  def calculate_points 
+    self.forecasts.each do |forecast|
+      forecast_points = forecast.calculate_points 
+      
+      point = Point.find_by(player_id: forecast.player_id, tournament_id: forecast.tournament_id)
+      
+      point.total_points += forecast_points - forecast.old_points
+      
+      point.save
+    end
+  end
+
+  def result_or_goals_changed? 
+    self.result_changed? || self.goals_home_changed? || self.goals_away_changed? 
+  end
+  
 
 end
