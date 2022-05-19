485c9e76b3f96482b04e55286cc4e8c9980676af
BrendaDichiara <brendy-15@hotmail.com>
2022-05-19 02:21:27 -0300

    Validate player

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,5 +4,9 @@ class Player < ActiveRecord::Base
     has_many :points 
     has_many :tournaments, through: :points
 
+    validates_presence_of :name, :email, :password
+    validates_uniqueness_of :name, :email
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
     has_secure_password
+    
   end
