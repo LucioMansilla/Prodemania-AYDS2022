bc9c9bac7de5057e8e3ba7cb678ce1f78f6ef496
LUCIOZTW <luciomansillaztw@gmail.com>
2022-06-01 13:02:01 -0300

    forecast_controller add

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,45 +1,49 @@
-require 'sinatra/reloader' if Sinatra::Base.environment == :development
-require 'logger'
-require_relative '../models/forecast'
-require 'sinatra/activerecord'
+class ForecastController < App
+    get '/forecasts' do
+        logger.info "123"
+        logger.info session
+        Forecast.first.to_json
+    end
 
+  ## ----forecasts controller----- ##
+  post '/forecasts' do
+    forecast = Forecast.new
+    forecast.match_id = params['match_id']
+    forecast.player_id = session['player_id']
+    forecast.result = params['result']
+    forecast.home_goals = params['home_goals']
+    forecast.away_goals = params['away_goals']
+    forecast.tournament_id = params['tournament_id']
+    logger.info(params)
+    if(!forecast.check_match_player && forecast.resultConsist) then
+        forecast.save
+        redirect '/matches?match_day_id=' + forecast.match.match_day_id.to_s
+    else
+        status 400
+        {:status => 400, :mge => "There is already a prediction for that match"}.to_json
+    end
+end
 
-class Forecast_Controller < Sinatra::Base
-    post '/forecast' do
-        forecast = Forecast.new
-        forecast.match_id = params['match_id']
-        forecast.player_id = params['player_id']
+put '/forecasts/:id' do
+    forecast = Forecast.find_by(id: params['id'])
+    if(forecast) then
         forecast.result = params['result']
         forecast.home_goals = params['home_goals']
         forecast.away_goals = params['away_goals']
         forecast.tournament_id = params['tournament_id']
         if(!forecast.check_match_player && forecast.resultConsist) then
+            forecast.points = 0
             forecast.save
+             
         else
             status 400
             {:status => 400, :mge => "There is already a prediction for that match"}.to_json
         end
+    else
+        status 400
+        {:status => 404, :mge => "This forecast doesn't exists"}.to_json
     end
-
-    put '/forecast/:id' do
-        forecast = Forecast.find_by(id: params['id'])
-        if(forecast) then
-            forecast.result = params['result']
-            forecast.home_goals = params['home_goals']
-            forecast.away_goals = params['away_goals']
-            forecast.tournament_id = params['tournament_id']
-            if(!forecast.check_match_player && forecast.resultConsist) then
-                forecast.points = 0
-                forecast.save
-            else
-                status 400
-                {:status => 400, :mge => "There is already a prediction for that match"}.to_json
-            end
-        else
-            status 400
-            {:status => 404, :mge => "This forecast doesn't exists"}.to_json
-        end
-    end
+end
 
 end
 
