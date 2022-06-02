14b825e07cfc151d9a27c9b0e8723051eaef6ea6
LUCIOZTW <luciomansillaztw@gmail.com>
2022-06-02 00:52:11 -0300

    correct erb

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,6 +1,9 @@
+#Module for admin helper 
 module AdminHelper
 
+    #Method admin_menu, returns the admin menu for the admin panel
     def get_menu_admin
-        "Test"
+        erb :"/admin/admin_menu"
     end
+
 end 
