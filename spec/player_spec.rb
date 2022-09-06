18eacda6e344aabfacc5f16bfe4d95eef9b16f8c
LucioMansilla <luciomansillaztw@gmail.com>
2022-09-06 11:07:26 -0300

    User-register-test added

---
+++ spec/player_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../app/models/init'
+
+=begin  #Task: Test-register-user
+    Si se intenta registrar un usuario ya existente - retornar el error correspondiente y hacer un rollback de la operación.
+
+    Si se intenta registrar un usuario con nombres inválidos - retornar el error correspondiente y hacer un rollback de la operación.
+
+    Si se intenta registrar un usuario con formato inválido de contraseña - retornar el error correspondiente y hacer un rollback de la operación.
+=end
+
+user = Player.new(name:"Lucio", email: "lucio@correo.com", password: "12345", is_admin: true)
+
+describe 'Player' do
+    describe 'when trying to register an existing user'  do
+        it 'should return error when user already exists' do
+            user_repeated = Player.new(name:"Lucio", email: "lucio@correo.com", password: "12345", is_admin: true)
+            expect(user.save).to eq(false)
+        end
+    end
+
+    describe 'when a user tries to register with invalid names' do
+        it 'should return false' do
+            user_invalid_name = Player.new(name:"L", email: "lucio@correo.com", password: "12345", is_admin: true)
+            expect(user.save).to eq(false)
+        end
+    end
+
+    describe 'when a user is registered with an invalid password format' do
+        it 'should return false' do
+            user_invalid_password = Player.new(name:"Lucio", email: "lucio@correo.com", password: "short", is_admin: true)
+            expect(user.save).to eq(false)
+        end
+    end
+end
