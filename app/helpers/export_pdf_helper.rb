0d9fd25e87fe96ea5ffaa2eabf7c648f31250f9b
BrendaDichiara <brendy-15@hotmail.com>
2022-09-26 21:03:51 -0300

    Add style pdf

--- app/helpers/export_pdf_helper.rb
+++ app/helpers/export_pdf_helper.rb
@@ -45,21 +45,42 @@ module ExportPdfHelper
     def create_pdf(data)
 
         content_type 'application/pdf'
-        pdf = Prawn::Document.new
-        #pdf = Prawn::Document.new(
-         #   background: "#242424"
-          #)
+        pdf = Prawn::Document.new()
 
+        pdf.canvas do
+          pdf.fill_color "242424"
+          pdf.fill_rectangle [pdf.bounds.left, pdf.bounds.top], pdf.bounds.right, pdf.bounds.top
+        end
+         
+        pdf.image open("public/images/logo.png"),:position => :left,:width=>300
         content = data.title
         pdf.font "Times-Roman"
-        pdf.fill_color "3aa17a"
-        pdf.draw_text content, :at => [150,720], :size => 30
-        pdf.move_down 20
         pdf.fill_color "f2f2f2"
-        pdf.table(
-        data.table, :width => 500, :cell_style => {
-          :font_style => :bold, :background_color => '242424', :border_color => '3aa17a'  } )
-        pdf.render
+        pdf.move_down 20
+        pdf.text content, :align => :center, :size => 30, :style => :bold, :margin => [0, 100, 0, 0]
+        pdf.move_down 20
+
+        pdf.table(data.table, :position => :center, :width => 500, :cell_style => { :size => 10, :font => "Helvetica",:font_style => :bold, :border_color => "3aa17a", :padding => 12, :align => :center, :inline_format => true }) do
+          for i in 0..data.table.length-1
+            if i == 0
+              row(i).background_color = "3aa17a"
+              row(i).border_color = "242424"
+              row(i).text_color = "242424"
+            else
+              row(i).background_color = "242424"
+              row(i).text_color = "f2f2f2"
+            end
+
+          end
+            
+        end
+  
+
+      pdf.bounding_box([pdf.bounds.left, pdf.bounds.bottom + 50], :width  => pdf.bounds.width) do
+        pdf.text "© 2022 - Todos los derechos reservados a Prodemania", :align => :center, :size => 12, :style => :bold, :margin => [0, 0, 0, 0] 
+      end
+
+      pdf.render
 
     end
     
